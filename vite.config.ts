ce6917cd3bc99f2e69cbef76adf0cd841148e543
texas2010 <9701609+texas2010@users.noreply.github.com>
2025-11-18 22:23:10 -0600

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 
-const viteConfig = process.env.VITECONFIG ?? 'common';
+const viteConfig = process.env.VITECONFIG ?? 'shared';
 
 const entryMap: Record<string, string> = {
-  index: resolve(__dirname, 'src/common/index.ts'),
-  common: resolve(__dirname, 'src/common/index.ts'),
-  react: resolve(__dirname, 'src/react/index.ts'),
+  shared: resolve(__dirname, 'src/shared/index.ts'),
+  backend: resolve(__dirname, 'src/backend/index.ts'),
+  frontend: resolve(__dirname, 'src/frontend/index.ts'),
+  react: resolve(__dirname, 'src/frontend/react/index.ts'),
 };
 
 if (!entryMap[viteConfig]) {
@@ -19,11 +20,11 @@ export default defineConfig({
   plugins: [
     viteConfig === 'react' ? react() : null,
     dts({
-      outDir: viteConfig === 'index' ? 'build' : `build/${viteConfig}`,
+      outDir: `build/${viteConfig}`,
       entryRoot: `src/${viteConfig}`,
       include: [`src/${viteConfig}`],
       insertTypesEntry: true,
-      // rollupTypes: true,
+      rollupTypes: viteConfig === 'shared' ? true : false,
     }),
   ].filter(Boolean),
 
@@ -35,7 +36,7 @@ export default defineConfig({
       formats: ['es'],
       fileName: () => `index.js`,
     },
-    outDir: viteConfig === 'index' ? 'build' : `build/${viteConfig}`,
+    outDir: `build/${viteConfig}`,
     rollupOptions: {
       external:
         viteConfig === 'react'
